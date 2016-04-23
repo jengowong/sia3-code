2270ea5f8ea4a628647186a3057a4d46d4d8246b
wangzhenguo02 <wangzhenguo02@meituan.com>
2016-04-23 19:46:19 +0800

    service-tx-programmatic

--- service-tx/src/test/java/com/habuma/spitter/service/SpitterServiceImplTest_Programmatic.java
+++ service-tx/src/test/java/com/habuma/spitter/service/SpitterServiceImplTest_Programmatic.java
@@ -4,20 +4,26 @@ import com.habuma.spitter.domain.Spitter;
 import com.habuma.spitter.persistence.SpitterDao;
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import static org.easymock.EasyMock.createNiceMock;
 import static org.easymock.EasyMock.replay;
 import static org.easymock.EasyMock.verify;
 
-public class SpitterServiceImplTest_Declarative {
+public class SpitterServiceImplTest_Programmatic {
 
-    private SpitterServiceImpl_Declarative spitterService;
+    private SpitterServiceImpl_Programmatic spitterService;
     private SpitterDao spitterDao;
     private Spitter newSpitter;
     private Spitter oldSpitter;
 
     @Before
     public void setup() {
+        PlatformTransactionManager txMgr = new FakeTransactionManager_Programmatic();
+
+        TransactionTemplate txTemplate = new TransactionTemplate(txMgr);
+
         newSpitter = new Spitter();
         newSpitter.setUsername("testuser");
         newSpitter.setPassword("password");
@@ -34,8 +40,9 @@ public class SpitterServiceImplTest_Declarative {
         spitterDao.addSpitter(newSpitter);
         replay(spitterDao);
 
-        spitterService = new SpitterServiceImpl_Declarative();
-        spitterService.spitterDao = spitterDao;
+        spitterService = new SpitterServiceImpl_Programmatic();
+        spitterService.setSpitterDao(spitterDao);
+        spitterService.setTransactionTemplate(txTemplate);
     }
 
     @Test
